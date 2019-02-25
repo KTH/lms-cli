90766309108c3036c44b4ecf811627922d8c7d1d
Carlos Saito <exacs@users.noreply.github.com>
2019-02-25 16:22:05 +0100

    Show number of messages in normal and dead-letter queues

--- deadLetters/index.js
+++ deadLetters/index.js
@@ -1,4 +1,19 @@
+require('dotenv').config()
+const { promisify } = require('util')
+const azure = require('azure-sb')
+
 async function start () {
+  const connectionString = process.env.AZURE_SERVICEBUS_CONNECTION_STRING
+  const topicName = process.env.AZURE_SERVICEBUS_TOPIC_NAME
+  const subscriptionName = process.env.AZURE_SERVICEBUS_SUBSCRIPTION_NAME
+
+  const service = azure.createServiceBusService(connectionString)
+  const getSubscription = promisify(service.getSubscription.bind(service))
+
+  const r = await getSubscription(topicName, subscriptionName)
+
+  console.log(`Messages in normal queue: ${r.CountDetails['d3p1:ActiveMessageCount']}`)
+  console.log(`Messages in DLQ:          ${r.CountDetails['d3p1:DeadLetterMessageCount']}`)
 }
 
 start()
