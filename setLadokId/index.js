28b3be01a33766fed8a46a697ad8aab195026e17
Carlos Saito <exacs@users.noreply.github.com>
2019-03-21 14:18:48 +0100

    Traverse for users in a section

--- setLadokId/index.js
+++ setLadokId/index.js
@@ -1,6 +1,15 @@
-function start () {
+require('dotenv').config()
+const canvas = require('@kth/canvas-api')(process.env.CANVAS_API_URL, process.env.CANVAS_API_TOKEN, {log: console.log})
+
+async function start () {
   console.log('Set LadokID (obtained from UG) to all users in a canvas course round (section)')
   console.log()
+
+  const sectionId = 'LT1016VT191'
+
+  for await (const enrollment of canvas.list(`sections/sis_section_id:${sectionId}/enrollments`)) {
+    console.log(enrollment.user.sis_user_id)
+  }
 }
 
 start()
