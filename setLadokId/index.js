10922fa0fd5d09b1e5680daae901d71e38cc1d52
Carlos Saito <exacs@users.noreply.github.com>
2019-03-21 15:15:00 +0100

    Search for the KTH ID in UG

--- setLadokId/index.js
+++ setLadokId/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
-const canvas = require('@kth/canvas-api')(process.env.CANVAS_API_URL, process.env.CANVAS_API_TOKEN, {log: console.log})
+const canvas = require('@kth/canvas-api')(process.env.CANVAS_API_URL, process.env.CANVAS_API_TOKEN)
+const ldap = require('../lib/ldap')
 
 async function start () {
   console.log('Set LadokID (obtained from UG) to all users in a canvas course round (section)')
@@ -7,9 +8,19 @@ async function start () {
 
   const sectionId = 'LT1016VT191'
 
+  await ldap.connect()
+
   for await (const enrollment of canvas.list(`sections/sis_section_id:${sectionId}/enrollments`)) {
-    console.log(enrollment.user.sis_user_id)
+    const kthId = enrollment.user.sis_user_id
+
+    if (kthId) {
+      const a = await ldap.search(`(ugKthId=${kthId})`)
+      console.log(kthId, a[0].ugLadok3StudentUid)
+    }
+
   }
+
+  await ldap.disconnect()
 }
 
 start()
