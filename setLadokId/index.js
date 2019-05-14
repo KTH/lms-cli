3f231909e987826585d6aa040e6e45a4830bdb80
Rasmus Kaj <kaj@kth.se>
2019-05-14 11:01:31 +0200

    Add some error handling.

--- setLadokId/index.js
+++ setLadokId/index.js
@@ -3,28 +3,36 @@ const canvas = require('@kth/canvas-api')(process.env.CANVAS_API_URL, process.en
 const ldap = require('../lib/ldap')
 
 async function start () {
-  console.log('Set LadokID (obtained from UG) to all users in a canvas course round (section)')
-  console.log()
+  try {
+    console.log('Set LadokID (obtained from UG) to all users in a canvas course round (section)')
+    console.log()
 
-  const sectionId = 'LT1016VT191'
+    const sectionId = 'LT1016VT191'
 
-  await ldap.connect()
+    await ldap.connect()
 
-  for await (const enrollment of canvas.list(`sections/sis_section_id:${sectionId}/enrollments`)) {
-    const kthId = enrollment.user.sis_user_id
+    for await (const enrollment of canvas.list(`sections/sis_section_id:${sectionId}/enrollments`)) {
+      const kthId = enrollment.user.sis_user_id
 
-    if (kthId) {
-      const [user] = await ldap.search(`(ugKthId=${kthId})`, ['ugLadok3StudentUid'])
-      const ladokId = user.ugLadok3StudentUid
+      if (kthId) {
+        const [user] = await ldap.search(`(ugKthId=${kthId})`, ['ugLadok3StudentUid'])
+        const ladokId = user.ugLadok3StudentUid
 
-      await canvas.requestUrl(`/users/sis_user_id:${kthId}/custom_data/ladok_uid`, 'PUT', {
-        ns: 'se.kth',
-        data: ladokId
-      })
+        await canvas.requestUrl(`/users/sis_user_id:${kthId}/custom_data/ladok_uid`, 'PUT', {
+          ns: 'se.kth',
+          data: ladokId
+        })
+      }
     }
-  }
 
-  await ldap.disconnect()
+  } catch (e) {
+    console.log("Error:", e)
+  }
+  try {
+    await ldap.disconnect()
+  } catch (e) {
+    console.log("Error:", e)
+  }
 }
 
 start()
