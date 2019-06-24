0e0409a5e76b031f85a2d9514cbae2a2a578c2a6
Emil <emilsten@kth.se>
2019-06-24 11:09:52 +0200

    list numbers

--- get-user-stats/index.js
+++ get-user-stats/index.js
@@ -2,10 +2,16 @@ require('dotenv').config()
 const Canvas = require('@kth/canvas-api')
 const canvas = Canvas(process.env.CANVAS_API_URL, process.env.CANVAS_API_TOKEN)
 
+let total = 0
+let hasIntegrationId = 0
 async function main () {
-  for await (const user of canvas.list(`/accounts/1/users?per_page=3`)) {
-    console.log(user)
-    console.log('------')
+  for await (const user of canvas.list(`/accounts/1/users?per_page=100`)) {
+    if (user.integration_id) {
+      hasIntegrationId++
+    }
+    total++
+
+    console.log(`${hasIntegrationId} users out of ${total} checked users has integration_id set`)
   }
 }
 main()
