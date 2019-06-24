4a8010c737a2e93dc73a95087d43793bc4a16871
Emil <emilsten@kth.se>
2019-06-24 11:06:18 +0200

    list all users

--- get-user-stats/index.js
+++ get-user-stats/index.js
@@ -3,8 +3,9 @@ const Canvas = require('@kth/canvas-api')
 const canvas = Canvas(process.env.CANVAS_API_URL, process.env.CANVAS_API_TOKEN)
 
 async function main () {
-  for await (const user of canvas.list(`/accounts/1/users?per_page=100`)) {
+  for await (const user of canvas.list(`/accounts/1/users?per_page=3`)) {
     console.log(user)
+    console.log('------')
   }
 }
 main()
