b22801d2f4d7edc9bcf72db0ed82945318488308
Emil <emilsten@kth.se>
2019-06-24 11:05:56 +0200

    list all users

--- get-user-stats/index.js
+++ get-user-stats/index.js
@@ -1,1 +1,10 @@
+require('dotenv').config()
 const Canvas = require('@kth/canvas-api')
+const canvas = Canvas(process.env.CANVAS_API_URL, process.env.CANVAS_API_TOKEN)
+
+async function main () {
+  for await (const user of canvas.list(`/accounts/1/users?per_page=100`)) {
+    console.log(user)
+  }
+}
+main()
