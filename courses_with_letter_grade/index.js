d256dc9763100dc923ba3ec4b2555013ba97d232
Emil Stenberg <esandin@gmail.com>
2019-12-17 21:23:31 +0100

    counting the courses with assignment letter grade

--- courses_with_letter_grade/index.js
+++ courses_with_letter_grade/index.js
@@ -1,12 +1,20 @@
 require('dotenv').config()
 
 const canvas = require('@kth/canvas-api')(process.env.CANVAS_API_URL, process.env.CANVAS_API_TOKEN)
-
+let count = 0
+let totalHT19 = 0
 async function get(){
-    const courses = canvas.list(`/accounts/1/courses`)
+    const courses = canvas.list(`/accounts/1/courses`, {per_page: 100, page:100})
     for await (const course of courses) {
+        totalHT19 ++
         if(course.sis_course_id && course.sis_course_id.match(/.*HT19.*/)){
-            console.log('found one:', course)
+            for await (const assignment of canvas.list(`courses/${course.id}/assignments`)){
+                if(assignment.grading_type === 'letter_grade'){
+                    count ++
+                    console.log('Found one for course:', course.sis_course_id, count, totalHT19)
+                    break
+                }
+            }
         }
     }
 
