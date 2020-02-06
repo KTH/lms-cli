1ca032350e30038769de3e80d8400b57970e8758
Emil <emilsten@kth.se>
2020-02-06 13:00:50 +0100

    also displaying disabled users enrollments

--- countDisabledUsers/index.js
+++ countDisabledUsers/index.js
@@ -1,31 +1,31 @@
 require('dotenv').config()
+require('colors')
 const canvas = require('@kth/canvas-api')(process.env.CANVAS_API_URL, process.env.CANVAS_API_TOKEN)
 let totalUsersChecked = 0
 let disabledUsers = 0
 async function doIt(){
     const ldap = require('../lib/ldap')
-    try {
       await ldap.connect()
       for await (const user of canvas.list(`/accounts/1/users` )) {
          const kthId = user.sis_user_id
+       // const kthId = 'u1cssscc' 
         if (kthId) {
 
         totalUsersChecked ++
           const [ldapUser] = await ldap.search(`(ugKthId=${kthId})`, [])
-          if (!ldapUser) {
-            throw new Error(`No user found for ${kthId}`)
-          }
-          // TODO: check disabled in UG
-          if(ldapUser.ugDisabled === 'true'){
+          if(!ldapUser || ldapUser.ugDisabled === 'true'){
             disabledUsers ++ 
 
+            // list enrollments
+            const {body:enrollments}= await canvas.get(`/users/sis_user_id:${kthId}/enrollments`)
+            if(enrollments.length){
+              console.log('User has enrollments'.redBG) 
+            }
+            console.log(`enrollments for user ${kthId}:`, enrollments)
           }
-          console.log(`${Math.round(disabledUsers/totalUsersChecked*100000)/1000}% of ${totalUsersChecked} users are disabled (${disabledUsers})`)
+          console.log(`${Math.round(disabledUsers/totalUsersChecked*100000)/1000}% of ${totalUsersChecked} users are disabled (${disabledUsers})`.green)
         }
       }
-    } catch (e) {
-      console.log('Error:', e)
-    }
     try {
       await ldap.disconnect()
     } catch (e) {
